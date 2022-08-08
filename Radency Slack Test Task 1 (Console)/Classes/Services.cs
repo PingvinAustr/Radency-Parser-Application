85d4572076f0204c7f904afc80c77300a67f8dd4
Yaroslav <yarik.chushenko1@gmail.com>
2022-08-08 13:24:55 +0300

    Subclass for JSON parse

---
+++ Radency Slack Test Task 1 (Console)/Classes/Services.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radency_Slack_Test_Task_1__Console_.Classes
+{
+    internal class Services
+    {
+        public string service { get; set; }
+    }
+}
